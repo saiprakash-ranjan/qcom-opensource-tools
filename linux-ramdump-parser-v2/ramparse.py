be9942e92e69e66530a6db3a9843ac3a02aa0d0c
Patrick Daly <pdaly@codeaurora.org>
2016-04-26 20:33:12 -0700

    lrdp-v2: Change board_num to a string
    The board number is intended to be a human readable identification value.
    Allowing this to be a string is more flexible.
    
    CRs-Fixed: 1013327
    Change-Id: If523acafc54f5026680d18c95e4899fb0436599d

--- linux-ramdump-parser-v2/ramparse.py
+++ linux-ramdump-parser-v2/ramparse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-# Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
+# Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -115,7 +115,7 @@ if __name__ == '__main__':
                       help='Offset for address space layout randomization')
     parser.add_option('', '--page-offset', type='int',
                       dest='page_offset', help='use custom page offset')
-    parser.add_option('', '--force-hardware', type='int',
+    parser.add_option('', '--force-hardware',
                       dest='force_hardware', help='Force the hardware detection')
     parser.add_option(
         '', '--force-version', type='int', dest='force_hardware_version',
