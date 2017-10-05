46401503913c4a735f8d1ba916b2227e94891ba8
Ankur Bansal <ankban@codeaurora.org>
2017-10-05 15:58:42 +0530

    lrdp_v2 : fix list walk function in linux_list.py
    fix list walk function in linux_list.py
    
    Change-Id: I78edb68c57c6623fca39266d30342688e1a5cf44

--- linux-ramdump-parser-v2/linux_list.py
+++ linux-ramdump-parser-v2/linux_list.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013-2014, 2016, The Linux Foundation. All rights reserved.
+# Copyright (c) 2013-2014, 2016-2017, The Linux Foundation. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -78,7 +78,7 @@ class ListWalker(object):
             return
 
         while True:
-            if node_addr == 0:
+            if node_addr == 0 or node_addr == None :
                 break
 
             funcargs = [node_addr - self.list_elem_offset] + list(args)
